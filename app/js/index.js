9f5b0d7bd4170ac181873b97a7abbd0997b0ea75
pavel_shevchuk_d <pavel.shevchuk@sitesoft.ru>
2020-08-12 20:10:12 +0500

    init

---
+++ app/js/index.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill'
+import './fix'
+import '../vendors/vendors'
+import './helpers'
+import './common-libs'
+import './blocks'
+import '../svg-sprite'
+import '../scss/main.scss'
