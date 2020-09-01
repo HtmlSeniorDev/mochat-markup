182f5773fcc28ac0d1998dc60ff343c24f248677
pavel_shevchuk_d <pavel.shevchuk@sitesoft.ru>
2020-09-01 17:10:26 +0000

    Исправлена верстка шкалы прогресса

--- app/blocks/progress-rating/scripts.js
+++ app/blocks/progress-rating/scripts.js
@@ -1,3 +1,10 @@
 $(() => {
-
-})
+  const rating = $('.rating');
+  rating.find('input').each(function () {
+    const radio = $(this);
+    radio.on('click', function () {
+         const centerBlock = rating.find(`[data-value=${radio.attr('value')}]`);
+         console.log(centerBlock.attr('data-value'))
+    })
+  })
+});
