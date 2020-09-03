fa1b76511781364d771dca55f0314a4ed7440fbf
pavel_shevchuk_d <pavel.shevchuk@sitesoft.ru>
2020-09-03 17:07:42 +0500

    Добавлена логика работы шкалы с рейтингом Добавлен  лейбл с ошибкой в миксин rating-scale Добавлен required у инпутов

--- app/blocks/progress-rating/scripts.js
+++ app/blocks/progress-rating/scripts.js
@@ -1,83 +1,45 @@
 $(() => {
-    const rating = $('.rating');
-    const lineHidden = 1; // т.к последняя линия скрыта
-    rating.find('input').each(function () {
+  const rating = $('.rating').children('.rating__rating-wrapper');
+  const lineHidden = 1; // т.к последняя линия скрыта
+  rating.find('input').each(function () {
     const radio = $(this);
     radio.on('click', function () {
-         const ratingId = $(this).closest('.rating').attr('data-scale')
-         const centerBlock = rating.find(`[data-value=${radio.attr('value')}]`);
-         let checkedValue = centerBlock.attr('data-value')
-         const elementsLineList = $(`[data-scale=${ratingId}]`).children('.rating__line-val')
-
-      if (checkedValue<=2) {
-         elementsLineList.each(function () {
-           const line = $(this)
-           const lineCount = +line.attr('data-value') + lineHidden
-           let labelColor = $(`[data-scale=${ratingId}]`).children('.rating__center').find(`[data-color=${lineCount}]`)
-           const dataValueElem = elementsColored(checkedValue);
-           const isInclude = dataValueElem.includes(lineCount)
-           if (isInclude) {
-             line.css('background','#EECF43')
-             // labelColor.append('<style>label:before{color:#EECF43;}</style>'); //todo изменять цвет лейбла
-           }
-           else {
-             line.css('background','#DBE9C8')
-           }
-         })
-      }
-      else if (checkedValue<=4) {
-        elementsLineList.each(function () {
-          const line = $(this)
-          const dataValueElem = elementsColored(checkedValue);
-          const isInclude = dataValueElem.includes(+line.attr('data-value') + 1)
-          if (isInclude) {
-            line.css('background', '#DBCB47')
-          }
-          else {
-            line.css('background','#DBE9C8')
-          }
-        })
-      }
-      else if (checkedValue<=6) {
-        elementsLineList.each(function () {
-          const line = $(this)
-          const dataValueElem = elementsColored(checkedValue);
-          const isInclude = dataValueElem.includes(+line.attr('data-value') + 1)
-          if (isInclude) {
-            line.css('background', '#CAC84A')
-          }
-          else {
-            line.css('background','#DBE9C8')
-          }
-        })
-      }
-      else if (checkedValue<=8) {
-        elementsLineList.each(function () {
-          const line = $(this)
-          const dataValueElem = elementsColored(checkedValue);
-          const isInclude = dataValueElem.includes(+line.attr('data-value') + 1)
-          if (isInclude) {
-            line.css('background', '#BBC54D')
-          }
-          else {
-            line.css('background','#DBE9C8')
-          }
-        })
-      }
-      else  if (checkedValue<=10) {
-        elementsLineList.each(function () {
-        const line = $(this)
-        const dataValueElem = elementsColored(checkedValue);
-        const isInclude = dataValueElem.includes(+line.attr('data-value') + 1)
-        if (isInclude) {
-          line.css('background', '#90BC56')
+      const ratingId = $(this).closest('.rating').attr('data-scale') // текущая шкала с рейтингом
+      const centerBlock = rating.find(`[data-value=${radio.attr('value')}]`);
+      const scaleRating = $(`[data-scale=${ratingId}]`);
+      const checkedValue = centerBlock.attr('data-value') // значение выбранной радиокнопки
+      const elementsLineList = scaleRating.children('.rating__rating-wrapper').children('.rating__line-val') // cписок линий для раскраски
+      const LabelList = scaleRating.children('.rating__rating-wrapper').children('.rating__center').children(`label`)  // список лейблов
+      // очищаем все не выбранные лейблы
+      LabelList.each(function () {
+        const label = $(this)
+        if (radio.attr('value') !== label.attr('data-color')) {
+          label.removeClass('rating__extra-after rating__extra-after--color-1')
         }
-        else {
-          line.css('background','#DBE9C8')
-        }
-       })
-    }
-
+      })
+      scaleColoring(checkedValue)
+      function scaleColoring(checkedValue) {
+        let compareObject = colorComparator(+checkedValue)
+        let color = compareObject.line // цвет линии
+        let labelClass = compareObject.label // класс с цветом для лейбла
+        let labelColor = scaleRating.children('.rating__rating-wrapper').children('.rating__center').find(`[data-color=${checkedValue}]`)
+        labelColor.addClass(`rating__extra-after rating__extra-after--color-${labelClass}`)
+        elementsLineList.each(function () {
+          const line = $(this)
+          const lineCount = +line.attr('data-value') + lineHidden
+          const dataValueElem = elementsColored(checkedValue);
+          const isInclude = dataValueElem.includes(lineCount)
+          if (isInclude) {
+            let labelColor = scaleRating.children('.rating__rating-wrapper').children('.rating__center').find(`[data-color=${lineCount - lineHidden}]`)
+            labelColor.addClass("rating__label-color")
+            line.css('background', color)
+          } else {
+            let labelColor = scaleRating.children('.rating__rating-wrapper').children('.rating__center').find(`[data-color=${lineCount}]`)
+            labelColor.removeClass("rating__label-color")
+            line.css('background', '#DBE9C8')
+          }
+        })
+      }
     })
   })
 });
@@ -89,3 +51,18 @@ function elementsColored(highValue) {
   }
   return list
 }
+
+function colorComparator(value) {
+  if (value <= 2) {
+    return {line: '#EECF43', label: 1}
+  } else if (value <= 4) {
+    return {line: '#DBCB47', label: 2}
+  } else if (value <= 6) {
+    return {line: '#CAC84A', label: 3}
+  } else if (value <= 8) {
+    return {line: '#BBC54D', label: 4}
+  } else if (value <= 10) {
+    return {line: '#90BC56', label: 5}
+  }
+}
+
