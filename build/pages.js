a3a269aa9d7aacde6f7f7db4a0e5722bdd2e0dcc
pavel_shevchuk_d <pavel.shevchuk@sitesoft.ru>
2020-10-06 13:07:01 +0000

    Добавил скрипты для страниц

--- build/pages.js
+++ build/pages.js
@@ -6,7 +6,7 @@ const paths = require('./pathsApp')
 
 module.exports = [
   parts.page({
-    title: 'index.pug.html',
+    title: 'index.pug',
     path: 'pages/index',
     entry: {
       markup_pageList: path.join(paths.app, 'pug/pages/index')
@@ -68,5 +68,16 @@ module.exports = [
     template: path.join(paths.app, 'pug/pages/personal/index.pug'),
     // Подключаемые зависимости на страницу
     chunks: ['runtime', 'vendors', 'app', 'personal']
+  }),
+
+  parts.page({
+    title: 'corruption-position',
+    path: 'pages/corruption-position',
+    entry: {
+      markup_corruption: path.join(paths.app, 'pug/pages/corruption-position')
+    },
+    template: path.join(paths.app, 'pug/pages/corruption-position/index.pug'),
+    // Подключаемые зависимости на страницу
+    chunks: ['runtime', 'vendors', 'app', 'corruption-position']
   }),
 ]
